681555827a452d341f9ebc5123fa8331809c1840
Abhishek Das <abhidas17695@gmail.com>
2018-07-17 20:26:44 +0530

    Removed hack

--- README.md
+++ README.md
@@ -26,36 +26,22 @@
 * Go to https://dweb.me/arc/archive.org/details/commute -> and should work fine, with no interception of URL
 * Reload any of these URLs and behavior is as expected - i.e. back at the same URL.
 
-# Steps to build the extension
-
-* Download or clone the repo 
-* Run `npm install` on the root folder (the folder containing web.config)
-* Run `npm run-script build` on the root folder
-* The bundled js file is to be found in the folder dist
-* Please note that the folder scripts in dist is empty currently
-
 # Steps to sync the repo with the original dweb-objects and dweb-transports
 
 * Download the repos from https://github.com/internetarchive/dweb-objects and https://github.com/internetarchive/dweb-transports
-* Rename the folders to objects and transports
-* In objects folder in file Domain.js at line 177 replace the code with `chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-    chrome.tabs.update(tabs[0].id,{url:url.href}, function(){});
-});`
-* Rename package.json of objects folder to package-objects.json and do the same for transports folder.
-* Copy these 2 files and paste in the root folder
-* Run `npm install -g package-json-merge`
-* Run `package-json-merge package-objects.json package-transports.json > package.json`
-* Open the newly created package.json file and declare webpack as a devDependency
-* Run `npm install`
-* Run `npm run-script build` on the root folder to see build the updated bundle.js
+* Run `npm install` on both folders
+* In dweb-objects folder in file `Domain.js` at line 177 replace the code with `Domain.newURL=url.href;`
+* Run `npm run-script build` on dweb-objects
+* Copy `dweb-objects-bundle.js` and `dweb-transports-bundle.js` from the folder dist of both dweb-objects and dweb-transports and paste in the folder dist of the extension folder
+
 
 # Documentation
-__manifest.json declares `bundle.js` as background script:__
-
-* In `bootloader.js` any new URL loaded is detected on line 55. The new URL is then passed to the function `start()` 
-* In `start()` , variables `searchParams` and `verbose` are initialised. If the URL is not a Chrome internal page and if the URL starts with either https://dweb or http://dweb , `main()` is called with `url` as an argument
-* In `start()` the `require` caches are cleared. This is to prevent `Signature not verified error`.
+__manifest.json declares `dweb-transports-bundle.js`,`dweb-objects-bundle.js` and `bootloader.js` as background scripts:__
+* In `bootloader.js` any new URL loaded is detected on line 55. 
+* Variables `searchParams` and `verbose` are initialised. If the URL is not a Chrome internal page and if the URL starts with either https://dweb or http://dweb , `main()` is called with `url` as an argument
+* `main()` is an asynchronous function.If the promise is resolved then at line 62 of `bootloader.js` the new URL is loaded
 * In `main()` the name is passed to p_bootname() -> Domain.p_resolveAndBoot which walks the Domain tree retrieving records via IPFS or HTTP.
-* Finally in Leaf.p_boot (line 178 of `objects/Domain.js`) the new HTML is ready to be loaded
+* Finally in Leaf.p_boot (line 178 of `dweb-objects/Domain.js`) the new HTML is ready to be loaded. The URL of this new web page is assigned to Domain.newURL
 * The URL of the new HTML is `url.href`
+* In `bootloader.js` this new URL is accessed as `DwebObjects.Domain.newURL`
 
---
+++ dist/scripts/bootloader.js
@@ -0,0 +1,69 @@
+function statusupdate(args) {
+        console.log(...arguments);
+        //statusdiv = document.getElementById("statusdiv");
+        //statusdiv.appendChild(DwebObjects.utils.createElement("span", {}, ...arguments));
+    }
+    async function main(url) {
+        await DwebTransports.p_connect({
+            statuselement: document.getElementById("statuselement"),
+            transports: searchparams.getAll("transport")
+        });
+        let orighref = searchparams.get("url") || url;
+        if (!orighref.startsWith("file:///")) {
+            statusupdate("Loading URL: ", orighref);
+            let url = new URL(orighref);
+            let name;
+            let pathname = (url.pathname === "/") ? "" : url.pathname; // Remove extraneous / if added by browser
+            url.pathname = "";  // Leave empty as intended, will resolve to Domain or Leaf of the domain name
+            if (url.hostname === "dweb.me") {                                   // e.g. https://dweb.me/arc/archive.org/foo
+                name = pathname.substring(1);                                   // should be arc/ or ipfs/ or something like that
+            } else if (url.hostname.startsWith("dweb.")) {                      // e.g. https://dweb.archive.org/details/commute
+                name = ["arc/", url.hostname.substring(5), pathname].join("");  // arc/archive.org/details/commute
+            } else if (pathname.startsWith("/archive.org")) {                  // e.g. https://localhost:4244/archive.org/detais/commu
+                name = ["arc", pathname].join("");                              // arc/archive.org/details/commute
+            } else {
+                statusupdate("Unable to bootstrap ", orighref, " unrecognized pattern");
+                return false;  // have to change how promise is resolved or rejected
+            }
+            const search_supplied = url.search.slice(1); // Skip initial ?
+            await p_bootname(name, {search_supplied});
+            return true; // have to change how promise is resolved or rejected
+        }
+    }
+    async function p_bootname(name, {search_supplied=undefined}={}) {
+        //document.getElementById("urlorname").value = name;
+        statusupdate("Resolving name: ",name,);   // Appears after loading
+        try {
+            await DwebObjects.Domain.p_resolveAndBoot(name, {search_supplied, verbose})
+        } catch(err) {  // If cant resolve to leaf, or boot fails
+            console.error("Got error",err);
+            statusupdate(err.message);
+        }
+    }
+
+    async function p_bootfromfield(field) {
+        let nameandsearch = document.getElementById(field).value.split('?')
+        return await p_bootname(nameandsearch, {});
+    }
+    function changeURL(id,url){
+        chrome.tabs.update(id,{url:url}, function(){});
+    }
+    // Next few lines need explaining!  If passed an extra parameter url= then it will use that as the URL instead of ...bootloader.html
+    // This is only useful until we have the server returning this file for anything under dweb.archive.org
+    var searchparams = null;
+    var verbose = null;
+    chrome.webRequest.onBeforeRequest.addListener(function(details) {
+        var url=details.url;
+        //var url="https://dweb.archive.org";
+        if((typeof details.url)!='undefined' && (details.url.indexOf("dweb.me")<0) && !(url.indexOf("dweb.me")>=0)&& url.startsWith("http") && (url.indexOf("dweb.")>=0)){
+            searchparams = new URL(url).searchParams;
+            verbose = searchparams.get("verbose");
+            main(url).then(function(resolve){
+                changeURL(details.tabId,DwebObjects.Domain.newURL);
+            },function(reject){});
+            
+        }
+    }, {urls: ["<all_urls>"], types: ["main_frame"]},['blocking']);
+
+
+
