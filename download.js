36d37a132303d305c8557d77791f503ed133d575
Abhishek Das <abhidas17695@gmail.com>
2018-07-27 00:16:16 +0530

    Using download-git-repo to pull from abhidas fork

--- download.js
+++ download.js
@@ -1,16 +1,6 @@
-//
-//download('abhidas17695/dweb-ext', 'dist-dweb', function (err) {
-//});
-//
-//
-//download('abhidas17695/dweb-transports', 'dist-combo', function (err) {
-//    console.log(err);
-//});
-
-var Git = require("nodegit");
-
-Git.Clone("https://github.com/abhidas17695/dweb-ext", "./dist-dweb").then(function(repository) {
-  Git.Clone("https://github.com/abhidas17695/wayback-machine-chrome-AF-BF", "./dist-combo").then(function(repository) {
-  
+var download = require("download-git-repo");
+download('abhidas17695/dweb-ext', 'dist-dweb', function (err) {
+    download('abhidas17695/wayback-machine-chrome-AF-BF', 'dist-combo', function (err) {
 });
 });
+
