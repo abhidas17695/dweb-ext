03c7c0c466c7fbb6a764746a42a6206596022798
Abhishek Das <abhidas17695@gmail.com>
2018-08-16 22:21:16 +0530

    Pulling from internetarchive

--- download.js
+++ download.js
@@ -4,7 +4,7 @@ download('internetarchive/dweb-transports', 'dweb-transports', function (err) {
 download('internetarchive/dweb-objects', 'dweb-objects', function (err) {
 });
 
-download('abhidas17695/wayback-machine-chrome-AF-BF', 'dist-combo', function (err) {
+download('internetarchive/wayback-machine-chrome', 'dist-combo', function (err) {
 });
 
 
